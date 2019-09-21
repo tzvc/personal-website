09fe4d0e55ebb7eeef9556f5993c7e063b6bb87f
theochampion <contact.theochampion@gmail.com>
2019-09-21 20:38:55 +0200

    :lipstick: Constact page UI

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,20 +1,63 @@
 import React from "react";
+import styled from "styled-components";
 // components
 import PageTitle from "../components/PageTitle";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+import stackoverflow from "../assets/stackoverflow.png";
 
-const contact = `
-{
-	"name": Théo Champion
-	"location": Paris, FRANCE
-	"email_address": contact.theochampion@gmail.com
-	"phone_number": (+33)624651305
-}
+const SocialContainer = styled.div`
+	font-family: "Roboto Mono", monospace;
+	font-size: 1em;
+	font-weight: normal;
+`;
+const SocialItem = styled.img`
+	font-family: "Roboto Mono", monospace;
+	font-size: 1em;
+	font-weight: normal;
+	height: 2em;
+	margin-right: 1em;
+	margin-top: 1em;
+	&:hover {
+		opacity: 0.7;
+	}
+`;
+
+const ContactInfoBlock = styled.pre`
+	font-family: "Roboto Mono", monospace;
+	font-size: 1em;
+	font-weight: normal;
 `;
 
 const ContactPage = () => (
 	<>
 		<PageTitle>Contact</PageTitle>
-		<pre>{contact}</pre>
+		{/* <InfoTitle>Infos</InfoTitle> */}
+		<ContactInfoBlock>
+			<p>
+				Name: Théo Champion
+				<br />
+				Location: Paris, FRANCE
+				<br />
+				Email:{" "}
+				<a href="mailto:contact.theochampion@gmail.com">
+					contact.theochampion@gmail.com
+				</a>
+				<br />
+				Phone: (+33)624651305
+			</p>
+		</ContactInfoBlock>
+		<SocialContainer>
+			<a href="https://github.com/theochampion">
+				<SocialItem src={github}></SocialItem>
+			</a>
+			<a href="https://www.linkedin.com/in/theo-champion-22a43462/">
+				<SocialItem src={linkedin}></SocialItem>
+			</a>
+			<a href="https://stackoverflow.com/users/7252179/th%c3%a9o-champion">
+				<SocialItem src={stackoverflow}></SocialItem>
+			</a>
+		</SocialContainer>
 	</>
 );
 
